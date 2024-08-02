c22f7f6648285ab2f2ab6f660a3414ce9821efb9
Abduullaahh <hafizabdullahtahir@gmail.com>
2024-08-02 13:33:52 +0500

    initial commit

---
+++ 3D Model/3d-model/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState, Suspense } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
+import { Bot } from '../public/Bot'
+import Signin from './signin'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Canvas>
+        <ambientLight/>
+        <OrbitControls/>
+        <Suspense fallback={null}>
+          <Bot/>
+        </Suspense>
+      </Canvas>
+      <Signin/>
+    </>
+  )
+}
+
+export default App
